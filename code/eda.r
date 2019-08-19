b9dd6cffc7bfefd41f1e043c69b1ae536d9cafc3
Ben <ben.williams@alaska.gov>
2019-08-19 11:49:25 -0800

    update

--- code/eda.r
+++ code/eda.r
@@ -9,12 +9,28 @@ dsr1 <- read_csv("data/dsr_adfg_2000-2018_v1.csv")
 psr <- read_csv("data/psr_adfg_1985-1999_v1.csv")
 psr1 <- read_csv("data/psr_adfg_2000-2018_v1.csv")
 
-bind_rows(dsr, dsr1, psr, psr1)
+bind_rows(dsr, dsr1, psr, psr1) %>% 
+  rename_all(tolower) %>% 
+  dplyr::select(cfec = `cfec fishery code`,
+         pfshy = `permit serial number`,
+         weight = `whole weight (sum)`,
+         species = `species code`,
+         port, 
+         adfg,
+         date = `date of landing` ) %>% 
+    mutate(date = mdy_hm(date)) -> data
+
+data %>% 
+  ggplot(aes(date, weight)) + 
+  geom_point()
+  
+  
+data %>% 
+  filter(species==145, weight<500) %>% 
+  ggplot(aes(weight, fill = port)) + 
+  geom_density(alpha = .2)
 
 View(dsr)
 
 unique(dsr$Port)
 
-dsr %>% 
-  filter(`Whole Weight (sum)`<100, `Species Code`==145) %>% 
-  ggplot(aes(`Whole Weight (sum)`)) + geom_density()
